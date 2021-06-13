496f9c9845745066c838c68e8cf20bf8e47da04d
Christian Bryant <74256156+Christian-Bryant2020@users.noreply.github.com>
2021-06-13 02:07:30 -0500

    Post routes

--- routes/api.js
+++ routes/api.js
@@ -10,6 +10,24 @@ router.get("/notes", (req, res) => {
     })
 });
 
+router.post("/notes", (req, res) => {
+    const newNote = req.body;
 
+    newNote.id = id(),
+    console.log(newNote);
+
+    fs.readFile(`${__dirname}/../db/db.json`, (err, data) => {
+        if (err) throw err;
+
+        let savedNotes = JSON.parse(data);
+        console.log(savedNotes);
+        savedNotes.push(newNote);
+        fs.writeFile(`${__dirname}/../db/db.json`, JSON.stringify(savedNotes), (err, data) => {
+            if (err) throw err;
+            console.log("Saved note");
+            res.json(newNote);
+        })
+    })
+})
 
 module.exports = router;
